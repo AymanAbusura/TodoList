224c596e60c56e7bf1194d6ee31bb727b2dfb2b9
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-10-18 18:46:00 +0300

    Done

---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+export const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(value) {
+            addTodo(value);
+            setValue('');
+        }
+    }
+
+    return (
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input 
+                type='text' 
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                className='todo-input'
+                placeholder='What needs to be done?'
+            />
+
+            <button
+                type='submit'
+                className='todo-btn'
+            >
+                Add Task
+            </button>
+        </form>
+    )
+}
